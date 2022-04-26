c926841a6388f46ce71cf0bf8811e977afceeedb
abed.ahmad <abed.ahmad@campus.technion.ac.il>
2022-04-26 23:41:26 +0300

    final

---
+++ tests/AsciiArtTool.h
@@ -0,0 +1,11 @@
+#ifndef HW1_ASCIIARTTOOL_H
+#define HW1_ASCIIARTTOOL_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "RLEList.h"
+
+RLEList asciiArtRead(FILE* in_stream);
+RLEListResult asciiArtPrint(RLEList list, FILE *out_stream);
+RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream);
+#endif // HW1_ASCIIARTTOOL_H
